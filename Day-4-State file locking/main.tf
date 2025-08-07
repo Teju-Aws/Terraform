5e2f93217dedaf8c95b8a032b36ffc5d7ec3c044
Teju-Aws <tejaswinigundluru@gmail.com>
2025-08-07 14:31:21 +0530

    vdsfd

--- Day-4-State file locking/main.tf
+++ Day-4-State file locking/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
-  ami ="ami-0d54604676873b4ec"
-  instance_type="t2.nano"
+  ami = "ami-0d54604676873b4ec"
+  instance_type="t2.micro"
 tags ={
     Name ="Server"
 }
