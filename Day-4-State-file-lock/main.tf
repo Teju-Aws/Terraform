1cacd5e0a124b6c6e99a0ab86b7fe38e3feff741
Teju-Aws <tejaswinigundluru@gmail.com>
2025-08-08 17:06:30 +0530

    DFG

--- Day-4-State-file-lock/main.tf
+++ Day-4-State-file-lock/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "name" {
     ami="ami-0d0ad8bb301edb745"
-    instance_type="t2.medium"
+    instance_type="t2.micro"
     tags = { 
-        Name = "My-Server"
+        Name = "My"
     }
   
 }
