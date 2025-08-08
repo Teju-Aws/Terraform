32a3fd9862dc4f30faba21e049bb10fc86a38b3f
Teju-Aws <tejaswinigundluru@gmail.com>
2025-08-08 16:24:29 +0530

    hgc

--- Day-4-State-file-lock/backend.tf
+++ Day-4-State-file-lock/backend.tf
@@ -1,13 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-bucket-111111111"   # S3 bucket name
-    key            = "Day-4/terraform.tfstate"       # Path to store state file
-    region         = "ap-south-1"                  # AWS region
-    #use_lockfile   = true           
-    dynamodb_table = "Lock-Table"
-
+    key            = "Day-4/terraform.tfstate"      # Path to store state file
+    region         = "ap-south-1"                   # AWS region
+    dynamodb_table = "Lock-Table"                   # DynamoDB table name for locking
+    encrypt        = true                           # Optional, but recommended
   }
-
-
-
 }
